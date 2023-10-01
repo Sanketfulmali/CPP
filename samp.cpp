fb948c80d9f6cf253f87e37e0923426d581e84f6
Sanketfulmali <114637873+Sanketfulmali@users.noreply.github.com>
2023-10-01 11:10:12 +0530

    Add files via upload

---
+++ samp.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void strr(string st){
+    cout<<st<<endl;
+}
+
+int main(){
+    cout<<"hello"<<endl;
+    string s = "stk";
+    
+    strr(s+"D");
+
+
+    return 0;
+}
